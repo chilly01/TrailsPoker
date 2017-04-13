653de6c37e03a85076a95a1ce9f26c51f3c2cde4
Cody Hillyard <Cody Hillyard@10.0.0.10>
2017-04-13 12:16:28 -0600

    added fonts and navigation links

--- application/views/pages/main.php
+++ application/views/pages/main.php
@@ -26,50 +26,70 @@
                     <div class="col-sm-2"></div>
                     <div class="col-sm-8 text-left">
                     <div id="home">
-                     <div >
+                     <div class="ch_msg">
 				<h1>Cody Hillyard</h1>
 				<p>Born and raised in Logan Utah.  I spent my childhood learning how to do as many things that I could</p>
-			</div>
-			<div >
+                    </div>
+			<div class="ch_act">
                             <div class='list-group'>
-                                <a href='#' class='list-group-item'>Facebook</a>
-                                <a href='#' class='list-group-item'>Twitter</a>
-                                <a href='#' class='list-group-item'>Email</a>
+                                <a href='https://www.facebook.com/cody.hillyard' target="_blank" class='list-group-item'><i class="fa fa-facebook-square" aria-hidden="true"></i> Facebook</a>
+                                <a href='https://twitter.com/chillyard' target="_blank" class='list-group-item'><i class="fa fa-twitter-square" aria-hidden="true"></i> Twitter</a>
+                                <a data-toggle="modal" data-target="#myModal" class='list-group-item'><i class="fa fa-envelope-open" aria-hidden="true"></i> Email</a>
                             </div>
 			<span>This is place holder for the social media links</span>
 		</div>
 		</div>
 		<div id="resume" >
-			<div >
+			<div class="ch_msg">
 				<h1> Cody Hillyard</h1>
 				<p>This is a test of the programming and web site building skills you have</p>
 			</div>
-			<div >
+			<div class="ch_act">
 			<span>This is place holder for the social media links</span>
 		</div>
 		</div>
 		
 		
 		<div id="blog" >
-			<div>
+			<div class="ch_msg">
 				<h1> Cody Hillyard</h1>
 				<p>This is a test of the programming and web site building skills you have</p>
 			</div>
-			<div >
+			<div class="ch_act">
 			<span>This is place holder for the social media links</span>
 		</div>
 		</div>
 		
 		<div id="projects">
-			<div>
+			<div class="ch_msg">
 				<h1 > Cody Hillyard</h1>
 				<p>This is a test of the programming and web site building skills you have</p>
 			</div>
-			<div >
+			<div class="ch_act">
 			<span>This is place holder for the social media links</span>
 		</div>
 		</div>
             </div>
                     </div>
                 </div>
+
+<div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">My Personal Email</h4>
+        </div>
+        <div class="modal-body">
+          <p>You can email me at: codyhillyard@gmail.com</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
             	
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,6 +7,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 <link rel="stylesheet" href="<?= $this->config->item('image_url').'assets/' ?>css/main.css">
+<link rel="stylesheet" href="<?= $this->config->item('image_url').'assets/' ?>font-awesome/css/font-awesome.min.css">
 <head>
                 <title>Cody Hillyard</title>
         </head>
