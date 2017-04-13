67c68477fa100db7f03f78d4e6258c34ae1eab51
Cody Hillyard <Cody Hillyard@10.0.0.10>
2017-04-13 11:00:53 -0600

    updateing layout

--- application/views/pages/main.php
+++ application/views/pages/main.php
@@ -18,47 +18,58 @@
 			</ul>
 			</div></div>
 		</nav>
+            <div id="background_container" class="container-fluid">
+              
 		<div id="image_header">
-		</br></br></br></br></br></br></br></br></br></br>
-		</div>
-		<div id="home" class="container-fluid">
-			<div class="col-sm-8">
+                </div>
+                <div id="news_feed" class="row content">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8 text-left">
+                    <div id="home">
+                     <div >
 				<h1>Cody Hillyard</h1>
 				<p>Born and raised in Logan Utah.  I spent my childhood learning how to do as many things that I could</p>
 			</div>
-			<div class="col-sm-4">
+			<div >
+                            <div class='list-group'>
+                                <a href='#' class='list-group-item'>Facebook</a>
+                                <a href='#' class='list-group-item'>Twitter</a>
+                                <a href='#' class='list-group-item'>Email</a>
+                            </div>
 			<span>This is place holder for the social media links</span>
 		</div>
 		</div>
-		
-		<div id="resume" class="container-fluid">
-			<div class="col-sm-8">
+		<div id="resume" >
+			<div >
 				<h1> Cody Hillyard</h1>
 				<p>This is a test of the programming and web site building skills you have</p>
 			</div>
-			<div class="col-sm-4">
+			<div >
 			<span>This is place holder for the social media links</span>
 		</div>
 		</div>
 		
 		
-		<div id="blog" class="container-fluid">
-			<div class="col-sm-8">
+		<div id="blog" >
+			<div>
 				<h1> Cody Hillyard</h1>
 				<p>This is a test of the programming and web site building skills you have</p>
 			</div>
-			<div class="col-sm-4">
+			<div >
 			<span>This is place holder for the social media links</span>
 		</div>
 		</div>
 		
-		<div id="projects" class="container-fluid">
-			<div class="col-sm-8">
+		<div id="projects">
+			<div>
 				<h1 > Cody Hillyard</h1>
 				<p>This is a test of the programming and web site building skills you have</p>
 			</div>
-			<div class="col-sm-4">
+			<div >
 			<span>This is place holder for the social media links</span>
 		</div>
 		</div>
-		
+            </div>
+                    </div>
+                </div>
+            	
