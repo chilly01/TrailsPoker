d398f15f7f88e4fbb8fbf12e113bb009bf53154e
Cody Hillyard <Cody Hillyard@10.0.0.10>
2017-10-31 18:42:03 -0600

    standardize code

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,6 +1,6 @@
 <em>&copy; 2017 codyhillyard.com</em>
-<a href="<?= site_url().'pages/index/home' ?>">Poker Main Page</a>
-<a href="<?= site_url().'pages/index/main' ?>">Developer Page</a>
+<a href="<?= site_url('pages/index/home') ?>">Poker Main Page</a>
+<a href="<?= site_url('pages/index/main') ?>">Developer Page</a>
 
 <?php
  if  ($this->session->active){ ?>
@@ -17,11 +17,7 @@ else {?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-<script>
-    $.get("https://ipinfo.io", function(response) {
-  console.log(response.ip, response.city, response.country);
-}, "jsonp"); 
-</script>
+
     
 </body>
 </html>
