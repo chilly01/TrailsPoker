343c08a7354887f6137c0bb585c4beb2b667788a
Cody Hillyard <cody@surefoot.com>
2017-12-14 21:46:25 -0700

    checking commit from new user

--- application/models/Model_events.php
+++ application/models/Model_events.php
@@ -2,7 +2,14 @@
 
 class Model_events extends CI_Model{
     
-    
+    /* there should be a table called events 
+
+     *  id (primary-int)
+     * name (varchar)
+     * date (date)
+     * notes (varchar)
+     * 
+     *      */
     function __construct()
     {
         // Call the Model constructor
@@ -37,7 +44,7 @@ class Model_events extends CI_Model{
     }
     
     function update_event($event, $points){
-        
+        return false; 
     }
     
 }
